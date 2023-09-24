b8b34232430401de03cc8fe317eb7c2459946108
VIPon4ik <umniyd@gmail.com>
2023-09-24 13:48:50 +0300

    Complete first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
+'use strict';
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+let galleryHtml = '';
+
+galleryItems.forEach((elem) => {
+    galleryHtml += `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${elem.original}">
+            <img
+                class="gallery__image"
+                src="${elem.preview}"
+                data-source="${elem.original}"
+                alt="${elem.description}"
+            />
+        </a>
+    </li>
+  `;
+});
+
+galleryList.insertAdjacentHTML("afterbegin",galleryHtml);
+
+// Opening functional
+
+galleryList.addEventListener('click', (event) => {
+    const imgElem = event.target.firstElementChild;
+    const instance = basicLightbox.create(`<img src="${imgElem.href}">`);
+
+    instance.show();
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            instance.close();
+        }
+    });
+});
